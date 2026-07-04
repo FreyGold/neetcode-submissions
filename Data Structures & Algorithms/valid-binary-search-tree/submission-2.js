19a2abf7d08405801d6b169739d9624d33eabbbe
Ahmed Tawfik <ahmedtawfik833@gmail.com>
2026-07-05 02:49:28 +0300

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValidBST(root) {
+        function traverse(node, min, max){
+            if(!node) return true;
+            if(node.val <= min || node.val >= max){
+                return false
+            }
+            return traverse(node.left, min, node.val) && traverse(node.right, node.val, max);
+
+            return true;
+        }
+        return traverse(root, -Infinity, Infinity)
+    }
+}
